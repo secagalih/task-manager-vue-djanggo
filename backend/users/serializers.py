c854734e168c190a926e6bde59a3fba36f01d007
secagalih <ysec0681@gmail.com>
2026-01-12 17:28:30 +0700

    Add total_task field to UserSerializer to count user tasks and remove unused import in views

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,8 +1,15 @@
 
 from rest_framework import serializers
 from .models import User
+from tasks.models import Task
 
 class UserSerializer(serializers.ModelSerializer):
+    total_task = serializers.SerializerMethodField()
+    
     class Meta:
         model = User
         fields = ['id', 'name', 'total_task', 'created_at', 'updated_at']
+    
+    def get_total_task(self, obj):
+
+        return Task.objects.filter(user=obj).count()
--- backend/users/views.py
+++ backend/users/views.py
@@ -4,7 +4,6 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import User
 from .serializers import UserSerializer
-from django.db.models import Count
 # Create your views here.
 
 class GetUsersView(APIView):
